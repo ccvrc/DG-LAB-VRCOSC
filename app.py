6ee00b69aa117b62fa95bea43a15ddbbf571255f
光水 <2079710627@qq.com>
2024-08-24 13:44:49 +0800

    style:修改注释，调整格式

--- app.py
+++ app.py
@@ -37,7 +37,6 @@ def print_qrcode(data: str):
     webbrowser.open('file://' + abs_path)
 
 
-
 def handle_osc_message_task_pad(address, list_object, *args):
     """
     将异步处理包装为同步调用，以便在 dispatcher 中使用. 实际就是创建新协程？
@@ -63,27 +62,33 @@ async def DGLab_Server():
         ipurl = f"ws://{local_ip}:5678"
         print(ipurl)
         client = server.new_local_client()
-        url = client.get_qrcode(ipurl)
+        url = client.get_qrcode(ipurl)  # 注意 PyCharm 开启时需要允许本地网络访问
         print("请用 DG-Lab App 扫描二维码以连接")
         print_qrcode(url)
-        
-        osc_client = udp_client.SimpleUDPClient("127.0.0.1", 9000)
+        # OSC 客户端用于发送回复        
+        osc_client = udp_client.SimpleUDPClient("127.0.0.1", 9000)  # 修改为接收 OSC 回复的目标 IP 和端口, 9000 为 VRChat 默认 OSC 传入接口
 
+        # OSC 服务器配置
         controller = DGLabController(client, osc_client)
+        # 注册需要进行处理的 OSC 参数，绑定回调
         disp = dispatcher.Dispatcher()
         disp.map("/avatar/parameters/SoundPad/Button/*", handle_osc_message_task_pad, controller)
         disp.map("/avatar/parameters/SoundPad/Volume", handle_osc_message_task_pad, controller)
         disp.map("/avatar/parameters/SoundPad/Page", handle_osc_message_task_pad, controller)
+        # TODO: 未开启动骨或 Contact 时，detach 这部分 OSC 地址？
         disp.map("/avatar/parameters/DG-LAB/*", handle_osc_message_task_pb, controller)
         disp.map("/avatar/parameters/Tail_Stretch", handle_osc_message_task_pb, controller)
 
         osc_server_instance = osc_server.AsyncIOOSCUDPServer(
             ("0.0.0.0", osc_port), disp, asyncio.get_event_loop()
+            # 修改为接收 OSC 回复的端口。9001 为 VRChat 的默认传出接口，为了兼容 VRCFT 面捕，这里通过 OSC Router 转换为 9102
         )
         osc_transport, osc_protocol = await osc_server_instance.create_serve_endpoint()
 
         logger.info("OSC Recv Serving on {}".format(osc_server_instance._server_address))
-
+        
+        # 等待绑定
+        # TODO 避免在客户端未连接时终端输出 OSC 信息
         await client.bind()
         logger.info(f"已与 App {client.target_id} 成功绑定")
 
